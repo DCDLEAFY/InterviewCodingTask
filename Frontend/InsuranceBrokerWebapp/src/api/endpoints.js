eb31b2d17706e0060d02e840106bce627cb72149
dcdleafy <allgaier.alif@gmail.com>
2022-09-27 21:03:21 +0100

    added Delete Customer func and altered previous string literals to use exported common const

--- Frontend/InsuranceBrokerWebapp/src/api/endpoints.js
+++ Frontend/InsuranceBrokerWebapp/src/api/endpoints.js
@@ -1,12 +1,16 @@
-import { url } from  "./common.js"
+import { url, apiCustomerPath} from  "./common.js"
 import axios from "axios"
 
 export function GetAllCustomers() {
     const headers = { "Content-Type": "application/json" };
-    return axios.get(url + "/api/Customer", {headers})
+    return axios.get(url + apiCustomerPath, {headers})
 }
 
 export function AddCustomer(customer){
     const headers = { "Content-Type": "application/json" };
-    return axios.post(url + "/api/Customer", customer, headers)
+    return axios.post(url + apiCustomerPath, customer, headers)
+}
+
+export function DeleteCustomer(id){
+    return axios.delete(url + apiCustomerPath + "?id=" + id);
 }
