96d8f635f155e61b417fd3589299b32fcb4197f3
dcdleafy <allgaier.alif@gmail.com>
2022-09-29 20:25:50 +0100

    Removed uneeded window.location.reload() calls

--- Frontend/InsuranceBrokerWebapp/src/App.vue
+++ Frontend/InsuranceBrokerWebapp/src/App.vue
@@ -1,7 +1,8 @@
 <script setup >
-import BrokerNav from './components/BrokerNavbar.vue'
-import CustomerTable from "./components//CustomerTable.vue"
-import Modal from "./components//Modal.vue"
+  //componants
+  import BrokerNav from './components/BrokerNavbar.vue'
+  import CustomerTable from "./components//CustomerTable.vue"
+  import Modal from "./components//Modal.vue"
 </script>
 
 
@@ -16,115 +17,114 @@ import Modal from "./components//Modal.vue"
     @filterstring="GetFilterSearch"/>
 
     <CustomerTable :clientsInfo="(filterSearch === '')? clientTableData : FilterTable" @EditCustomer="GetEditableRow"/>
-
   
-  
-  <Modal dynamicSubmit="AddNewCustomer" v-show="showAddModal" @Close="{showAddModal = !showAddModal;}" @AddNewCustomer="AddClientData">
-    <template v-slot:header>
-      Add Customer information to Database
-    </template>
-    <template v-slot:body>
-      <div class="form-group row">
-        <label class="col-sm-2 col-form-label" for="addName">Name</label>
-        <div class="col-sm-10">
-          <input type="text" class="form-control" id="addName" v-model="this.customer.name">
+    <Modal dynamicSubmit="AddNewCustomer" v-show="showAddModal" @Close="{showAddModal = !showAddModal;}" @AddNewCustomer="AddClientData">
+      <template v-slot:header>
+        Add Customer information to Database
+      </template>
+      <template v-slot:body>
+        <div class="form-group row">
+          <label class="col-sm-2 col-form-label" for="addName">Name</label>
+          <div class="col-sm-10">
+            <input type="text" class="form-control" id="addName" v-model="this.customer.name">
+          </div>
         </div>
-      </div>
-      <div class="form-group row">
-        <label class="col-sm-2 col-form-label" for="addAddress">Address</label>
-        <div class="col-sm-10">
-          <input type="text" class="form-control" id="addAddress" v-model="this.customer.address">
+        <div class="form-group row">
+          <label class="col-sm-2 col-form-label" for="addAddress">Address</label>
+          <div class="col-sm-10">
+            <input type="text" class="form-control" id="addAddress" v-model="this.customer.address">
+          </div>
         </div>
-      </div>
-      <div class="form-group row">
-        <label class="col-sm-2 col-form-label" for="addPolicy">PolicyType</label>
-        <div class="col-sm-10">
-          <input type="text" class="form-control" id="addPolicy" v-model="this.customer.policyType">
+        <div class="form-group row">
+          <label class="col-sm-2 col-form-label" for="addPolicy">PolicyType</label>
+          <div class="col-sm-10">
+            <input type="text" class="form-control" id="addPolicy" v-model="this.customer.policyType">
+          </div>
         </div>
-      </div>
-      <div class="form-group row">
-        <label class="col-sm-2 col-form-label" for="addInsurer">InsurerName</label>
-        <div class="col-sm-10">
-          <input type="text" class="form-control" id="addInsurer" v-model="this.customer.insurerName">
+        <div class="form-group row">
+          <label class="col-sm-2 col-form-label" for="addInsurer">InsurerName</label>
+          <div class="col-sm-10">
+            <input type="text" class="form-control" id="addInsurer" v-model="this.customer.insurerName">
+          </div>
         </div>
-      </div>
-      <div class="form-group row">
-        <label class="col-sm-2 col-form-label" for="addPremium">Premium</label>
-        <div class="col-sm-10">
-          <input type="number" class="form-control" id="addPremium" v-model="this.customer.premium">
+        <div class="form-group row">
+          <label class="col-sm-2 col-form-label" for="addPremium">Premium</label>
+          <div class="col-sm-10">
+            <input type="number" class="form-control" id="addPremium" v-model="this.customer.premium">
+          </div>
         </div>
-      </div>
-    </template>
-    <template v-slot:footer>
-    </template>
-  </Modal>
+      </template>
+      <template v-slot:footer>
+      </template>
+    </Modal>
 
 
-  <Modal dynamicSubmit="DeleteCustomer" v-show="showDeleteModal" @Close="{showDeleteModal = !showDeleteModal;}" @DeleteCustomer="DeleteClientData">
-    <template v-slot:header>
-      Enter Customer ID to delete
-    </template>
-    <template v-slot:body>
-      <div class="form-group row">
-        <label class="col-sm-2 col-form-label" for="deleteCustomerID">Customer ID</label>
-        <div class="col-sm-10">
-          <input type="text" class="form-control" id="deleteCustomerID" v-model="this.deleteId">
+    <Modal dynamicSubmit="DeleteCustomer" v-show="showDeleteModal" @Close="{showDeleteModal = !showDeleteModal;}" @DeleteCustomer="DeleteClientData">
+      <template v-slot:header>
+        Enter Customer ID to delete
+      </template>
+      <template v-slot:body>
+        <div class="form-group row">
+          <label class="col-sm-2 col-form-label" for="deleteCustomerID">Customer ID</label>
+          <div class="col-sm-10">
+            <input type="text" class="form-control" id="deleteCustomerID" v-model="this.deleteId">
+          </div>
         </div>
-      </div>
-    </template>
-  </Modal>
+      </template>
+    </Modal>
 
-  <Modal dynamicSubmit="EditCustomer" v-show="showEditModal" @Close="{showEditModal = !showEditModal;}" @EditCustomer="">
-    <template v-slot:header>
-      Edit Customer information
-    </template>
-    <template v-slot:body>
-      <div class="form-group row">
-        <label class="col-sm-2 col-form-label" for="editId">ID</label>
-        <div class="col-sm-10">
-          <input type="text" class="form-control" id="editId" v-model="this.customer.id" readonly>
+    <Modal dynamicSubmit="EditCustomer" v-show="showEditModal" @Close="{showEditModal = !showEditModal;}" @EditCustomer="UpdateClientData">
+      <template v-slot:header>
+        Edit Customer information
+      </template>
+      <template v-slot:body>
+        <div class="form-group row">
+          <label class="col-sm-2 col-form-label" for="editId">ID</label>
+          <div class="col-sm-10">
+            <input type="text" class="form-control" id="editId" v-model="this.customer.id" readonly>
+          </div>
         </div>
-      </div>
-      <div class="form-group row">
-        <label class="col-sm-2 col-form-label" for="addName">Name</label>
-        <div class="col-sm-10">
-          <input type="text" class="form-control" id="addName" v-model="this.customer.name">
+        <div class="form-group row">
+          <label class="col-sm-2 col-form-label" for="addName">Name</label>
+          <div class="col-sm-10">
+            <input type="text" class="form-control" id="addName" v-model="this.customer.name">
+          </div>
         </div>
-      </div>
-      <div class="form-group row">
-        <label class="col-sm-2 col-form-label" for="addAddress">Address</label>
-        <div class="col-sm-10">
-          <input type="text" class="form-control" id="addAddress" v-model="this.customer.address">
+        <div class="form-group row">
+          <label class="col-sm-2 col-form-label" for="addAddress">Address</label>
+          <div class="col-sm-10">
+            <input type="text" class="form-control" id="addAddress" v-model="this.customer.address">
+          </div>
         </div>
-      </div>
-      <div class="form-group row">
-        <label class="col-sm-2 col-form-label" for="addPolicy">PolicyType</label>
-        <div class="col-sm-10">
-          <input type="text" class="form-control" id="addPolicy" v-model="this.customer.policyType">
+        <div class="form-group row">
+          <label class="col-sm-2 col-form-label" for="addPolicy">PolicyType</label>
+          <div class="col-sm-10">
+            <input type="text" class="form-control" id="addPolicy" v-model="this.customer.policyType">
+          </div>
         </div>
-      </div>
-      <div class="form-group row">
-        <label class="col-sm-2 col-form-label" for="addInsurer">InsurerName</label>
-        <div class="col-sm-10">
-          <input type="text" class="form-control" id="addInsurer" v-model="this.customer.insurerName">
+        <div class="form-group row">
+          <label class="col-sm-2 col-form-label" for="addInsurer">InsurerName</label>
+          <div class="col-sm-10">
+            <input type="text" class="form-control" id="addInsurer" v-model="this.customer.insurerName">
+          </div>
         </div>
-      </div>
-      <div class="form-group row">
-        <label class="col-sm-2 col-form-label" for="addPremium">Premium</label>
-        <div class="col-sm-10">
-          <input type="text" class="form-control" id="addPremium" v-model="this.customer.premium">
+        <div class="form-group row">
+          <label class="col-sm-2 col-form-label" for="addPremium">Premium</label>
+          <div class="col-sm-10">
+            <input type="text" class="form-control" id="addPremium" v-model="this.customer.premium">
+          </div>
         </div>
-      </div>
-    </template>
-    <template v-slot:footer>
-    </template>
-  </Modal>
+      </template>
+      <template v-slot:footer>
+      </template>
+    </Modal>
   </main>
 </template>
 
 <script>
   import { AddCustomer, DeleteCustomer, GetAllCustomers, UpdateCustomer } from "./api/endpoints.js";
-
+  
+    
   export default {
     data(){
       return{
@@ -191,7 +191,6 @@ import Modal from "./components//Modal.vue"
       },
 
       AddClientData(){
-        this.showAddModal = !this.showAddModal
         AddCustomer(this.customer)
         .then((response) => {
           if(response.status !== 200){
@@ -199,10 +198,13 @@ import Modal from "./components//Modal.vue"
           }
         })
         .catch(err => alert(err))
+        .finally(()=>{
+          this.showAddModal = !this.showAddModal;
+          window.location.reload();
+        })
       },
 
       DeleteClientData(){
-        this.showDeleteModal = !this.showDeleteModal
         DeleteCustomer(this.deleteId)
         .then((response) => {
           if(response.status !== 200){
@@ -210,10 +212,23 @@ import Modal from "./components//Modal.vue"
           }
         })
         .catch(err => alert(err))
+        .finally(()=>{
+          this.showDeleteModal = !this.showDeleteModal;
+          window.location.reload();
+        })
       },
 
       UpdateClientData(){
-        
+        UpdateCustomer(this.customer)
+        .then((response) => {
+          if(response.status !== 200){
+            throw new Error("HTTP Status: " + response.status)
+          }
+        })
+        .catch(err => alert(err))
+        .finally(()=>{
+          this.showEditModal = !this.showEditModal;
+        })
       }
     }
   }  
