3c8f6a54f6918010a3beef682895c72443c451f6
dcdleafy <allgaier.alif@gmail.com>
2022-10-01 01:08:17 +0100

    COR's policy update to allow connection on same ip, and Dockerfile created

--- Backend/WebApi/Program.cs
+++ Backend/WebApi/Program.cs
@@ -2,18 +2,33 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("CorsPolicy", builder =>
+        builder.SetIsOriginAllowed(origin => true)
+       .AllowAnyMethod()
+       .AllowAnyHeader()
+       .AllowCredentials());
+});
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+app.UseHttpLogging();
+
+app.UseCors("CorsPolicy");
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+
+
 }
 
 app.UseHttpsRedirection();
